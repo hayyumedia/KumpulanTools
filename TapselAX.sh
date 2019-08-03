66c032a2e3cdf696d87c71fd4e61da6367ec3175
Mr_HackPro <44978328+Systemvuln@users.noreply.github.com>
2019-08-03 14:02:54 +0700

    Shell

---
+++ TapselAX.sh
@@ -0,0 +1,111 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+echo
+echo
+clear
+echo $i"TOOL INI TIDAK DI PUNGUT BIAYA!!!! "
+echo $i"SEMOGA ANDA SENANG DENGAN TOOLS KAMI"
+sleep 3
+clear
+echo $cy"++++++++++++++++++++++++++++++++++++++++++++"
+echo $cy"• TOOLS INSTALLER VIP"
+echo $cy"• UPDATE 23 MEI 2019"
+echo $cy"• NAME : TAPSEL 404"
+echo $cy"• TOOLS 310"
+echo $cy"• MR.F4K3"
+echo $cy"• TOOLS INI GRATIS!!!"
+echo $cy"++++++++++++++++++++++++++++++++++++++++++++"
+echo
+sleep 3
+echo $cy"[1]"$pu" Install Bahan"
+sleep 1
+echo $cy"[2]"$me" Tidak, saya sudah install bahan"
+sleep 1
+echo $cy"[3]"$i "EXIT"
+echo
+sleep 1
+echo $cy"["$i"H"$cy"]" $i"How To Get Username & Password"
+echo $cy"["$i"I"$cy"]" $i"INFO TOOLS"
+echo
+read -p "[SELECT NUMBER] : " pil;
+
+if [ $pil = 1 ]
+then
+clear
+pkg install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pip2 install termcolor
+pkg install ruby
+pkg install gem
+gem install lolcat
+pip2 install lolcat
+pkg install openssl
+pkg install php
+pkg install perl
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install figlet
+pkg install nano
+git clone https://github.com/LuciferXtools/TAPSEL404
+cd TAPSEL404
+sh TAPSEL404.sh
+fi
+
+if [ $pil = 2 ]
+then
+clear
+git clone https://github.com/LuciferXtools/TAPSEL404
+cd TAPSEL404
+sh TAPSEL404.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+echo "LOADING."
+sleep 1
+clear
+echo "LOADING.."
+sleep 1
+clear
+echo "LADING..."
+sleep 1
+clear
+echo "SUKSES"
+fi
+
+if [ $pil = H ]
+then
+clear
+echo $i"1. PM WA : 081260032271"
+sleep 2
+echo $i"2. SUBSCRIBE MY CHANNEL : MR.F4K3 YT"
+sleep 2
+echo $i"3. LINK CHANNEL : bit.ly/FakeYT"
+sleep 2
+echo $i"4. Like My video"
+sleep 2
+echo $i"5. Dan Kamu akan mendapat password"
+fi
+
+if [ $pil = I ]
+then
+clear
+echo $i"CREATED : JHOSUA SAUT MARULI"
+sleep 1
+echo $i"TYPE    : TOOLS INSTALLER"
+sleep 1
+echo $i"UPDATE  : 23 MEI 2019"
+sleep 1
+echo $i"TOTAL   : 310 TOOLS"
+fi
